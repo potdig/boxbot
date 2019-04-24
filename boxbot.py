27dc7cbd0fdfe5f770e45fc865c42ce9765b801c
potato digger <potadig.ashnerd.zino@gmail.com>
2019-04-25 01:17:41 +0900

    Box reset feature, decorate message, bug fix

--- boxbot.py
+++ boxbot.py
@@ -20,15 +20,11 @@ async def on_message(mes):
     # '/register'が来たらチャンネル登録
     if content.startswith("/register"):
         if channel in client.allowed_channels:
-            await channel.send("This channel is already registered!")
+            await channel.send(":no_entry_sign: **ERROR! This channel is already registered!**")
             return
         client.allowed_channels.append(channel)
-        await channel.send(f"I can speak in {channel.mention} from now! ;)")
+        await channel.send(f":tada: **I can speak in {channel.mention} from now! ;)**")
         return
-
-    # メッセージを送れるチャンネルかチェック
-    if channel not in client.allowed_channels:
-        await channel.send("This channel is not allowed to send message for me!")
 
     # メンバーのメンションを集めて箱を作る
     if channel.id not in client.boxes or client.boxes[channel.id].size() == 0:
@@ -44,14 +40,14 @@ async def on_message(mes):
             for removal in args[1:]:
                 print(removal)
                 box.remove(removal)
-                await channel.send(f"{removal} has removed from box")
-            await channel.send(f"{box.size()} remains in the box")
+                await channel.send(f":outbox_tray: **{removal} has removed from box**")
+            await channel.send(f"**{box.size()} remains in the box**")
         else:
             # 引数指定がない場合はランダムに引く
             picked = box.pick()
             print(picked)
-            await channel.send(f"{picked}, it's your turn")
-            await channel.send(f"{box.size()} remains in the box")
+            await channel.send(f":point_right: **{picked}, it's your turn**")
+            await channel.send(f"**{box.size()} remains in the box**")
     elif args[0] == "/team":
         # '/team'が来たらチーム分け
         if len(args) <= 1:
@@ -59,21 +55,31 @@ async def on_message(mes):
             count = 2
         else:
             if not args[1].isnumeric():
-                await channel.send("**ERROR!** Non-numeric argument!!")
+                await channel.send(":no_entry_sign: **ERROR! Non-numeric argument!!**")
                 return
             count = int(args[1])
+
+        if count > box.size():
+            await channel.send(":no_entry_sign: **ERROR! Team count is larger than amount in the box!!**")
+            return
+
         teams = box.team(count)
         for cnt in range(len(teams)):
             message = str(cnt + 1) + ": "
             message += " / ".join(teams[cnt])
             await channel.send(message)
+    elif args[0] == "/reset":
+        # '/reset'が来たら箱を作り直す
+        client.boxes[channel.id] = LotteryBox([ member.mention for member in channel.members if member.id != client.user.id])
+        await channel.send(":inbox_tray: **Box has reset.**")
     elif args[0] == "/help":
         help_message = '''```
 /pick [@mention...] : Pick from the box randomly. Add @mention to specify content.
 /team count : Make teams with contents in the box. Specify count.
+/reset : Reset the box.
 /help : Show this help.
 ```'''
-        await channel.send("")
+        await channel.send(help_message)
 
 
 with open('./token.txt') as f:
